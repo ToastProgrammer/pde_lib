ef986b9190551424eaebd6359134586ba66a9c60
BTozier <benjitenji@gmail.com>
2023-06-21 20:24:17 -0400

    clean up pde file and add pytest dep

--- pde_lib/calculation/pde.py
+++ pde_lib/calculation/pde.py
@@ -1,10 +1,7 @@
 """
 Functionality that involves finding solutions to Partial Differential Equations.
 """
-from dataclasses import dataclass, field
 import numpy as np
-import matplotlib.pyplot as plt
-from typing import Callable, Tuple, Union, Dict, Optional, Iterable
 
 
 def solve_fw_euler(
@@ -15,14 +12,14 @@ def solve_fw_euler(
         initial_condition: np.ndarray
 ) -> np.ndarray:
     """
-    Simulate the heat equation using the given parameters.
-    :param num_dx: the number of discrete points over length to use
-    :param num_dt: the number of discrete time points over time_total to evaluate
-    :param time_total: total period of time to solve over
-    :param diffusivity: the diffusion constant
-    :param initial_condition: the initial values of the system
+    Approximate solution for PDE using Forward-Euler method
+    :param num_dx: number of discrete points over length to use
+    :param num_dt: number of discrete points over time period to use
+    :param time_total: interval of time to use in modeling PDE solution
+    :param diffusivity: diffusion constant
+    :param initial_condition: initial values of the system at each x-position
 
-    :return: 2-D array of the system temperature at the given spatial and time intervals
+    :return: 2-D array of the system temperature at given [position, time] intervals
     """
     id_matrix = np.eye(num_dx - 1)     # (n-1) x (n-1) Identity matrix
     A = diffusivity * num_dx ** 2 * (
@@ -31,7 +28,7 @@ def solve_fw_euler(
     # B = delta_t * A + I
     B = (time_total / num_dt) * A + id_matrix
 
-    # Inital step
+    # Initial step
     u = np.zeros((num_dt - 1, num_dx - 1))
     u[0] = B @ initial_condition[1:-1]
     for time_step in range(1, num_dt-1):
